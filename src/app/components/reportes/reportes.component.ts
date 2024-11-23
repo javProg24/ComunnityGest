9c9885bc28323e09e3af54bd3ff5e0c633e000d0
JEAN MARCO ROMERO SERRANO <jean.romeros@ug.edu.ec>
2024-11-23 15:16:48 -0500

    Corregir en el Html

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -1,27 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Reporte } from '../../models/reportes.model';
 import { ReportesServiceService } from '../../services/reportes-services/reportes.service.service';
-import { MatCardContent } from '@angular/material/card';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatCell, MatHeaderCell, MatHeaderRow, MatRow, MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatCard } from '@angular/material/card';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-reportes',
-  imports: [],
+  imports: [MatFormField,MatLabel,MatCard,MatError,MatSelect,MatOption,MatTable,MatHeaderCell,MatCell,MatIcon,MatHeaderRow,MatRow],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
-export class ReportesComponent {
+export class ReportesComponent implements OnInit, AfterViewInit{
+filteredReportes: any;
+
+onSubmit() {
+throw new Error('Method not implemented.');
+}
   title = 'Reportes Comunitarios'
   reportes: Reporte[]=[];
-  constructor(private miServicioRep: ReportesServiceService ){
+  reporteForm!: FormGroup;
+  editMode = false;
+  currentId!:number;
+  dataSource = new MatTableDataSource<Reporte>();
+  notification: { message: string; type: 'info' | 'success' | 'error' | 'warning'  } = {
+    message: '',
+    type: 'info'
+  };
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(private miServicioRep: ReportesServiceService, private fb: FormBuilder){
   }  
+  
 
   ngOnInit():void{
     this.getReportes();
   }
 
   getReportes(): void{
-    this.miServicioRep.getReportes().subscribe((data : Reporte[])=>{
-      this.reportes = data;
+    this.miServicioRep.getReportes().subscribe((datos : Reporte[])=>{
+      this.dataSource.data = datos;
       console.log(this.reportes[0])
     });
   }
--- src/app/components/utils/mydialog/mydialog.component.ts
+++ src/app/components/utils/mydialog/mydialog.component.ts
@@ -1,24 +1,13 @@
 import { Component, Inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-export interface DialogPerfil{
-  nombre: string,
-  apellido: string,
-  link:string,
-  numero: string,
-  rol:string
-}
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-mydialog',
-  imports: [MatButtonModule,MatIconModule,MatDialogModule],
+  imports: [MatDialogActions, MatDialogContent, MatDialogClose, MatDialogModule, MatButtonModule, MatButton],
   templateUrl: './mydialog.component.html',
   styleUrl: './mydialog.component.css'
 })
 export class MydialogComponent {
-  constructor(public dialogRef:MatDialogRef<MatDialogModule>,
-    @Inject(MAT_DIALOG_DATA) public data:DialogPerfil){}
-    onCerrar():void{
-      this.dialogRef.close(true);
-    }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { titulo: string; mensaje: string }) {}
 }
