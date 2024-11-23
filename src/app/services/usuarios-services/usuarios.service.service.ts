21faa402a5bc8d3a908d6acbe3d8db514c83e200
Javier Alejandro TG <javiertomalag14@gmail.com>
2024-11-23 08:22:58 -0500

    Arregalndo usuarios

--- src/app/services/usuarios-services/usuarios.service.service.ts
+++ src/app/services/usuarios-services/usuarios.service.service.ts
@@ -1,36 +1,11 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Usuario } from '../../models/usuarios.model';
-
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosServiceService {
-  private apiUrl = 'http://localhost:3000/usuario';
+  
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  // Obtener todos los usuarios
-  getUsuarios(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(this.apiUrl);
-  }
-
-  // Simular creación de un usuario
-  addUsuario(usuarios: Usuario[], nuevoUsuario: Usuario): Usuario[] {
-    nuevoUsuario.id = usuarios.length > 0 ? Math.max(...usuarios.map(u => u.id)) + 1 : 1;
-    return [...usuarios, nuevoUsuario];
-  }
-
-  // Simular actualización de un usuario
-  updateUsuario(usuarios: Usuario[], usuarioEditado: Usuario): Usuario[] {
-    return usuarios.map(usuario =>
-      usuario.id === usuarioEditado.id ? { ...usuario, ...usuarioEditado } : usuario
-    );
-  }
-
-  // Simular eliminación de un usuario
-  deleteUsuario(usuarios: Usuario[], id: number): Usuario[] {
-    return usuarios.filter(usuario => usuario.id !== id);
-  }
+  
 }
